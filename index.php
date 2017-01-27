d340b717da261375ebee19113d7068d4d16feb9c
Zillmann <zillmann.szabolcs@gmail.com>
2017-01-27 02:09:19 +0200

    Basic html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Project Manager</title>
+    <link rel="stylesheet" type="text/css" href="_/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="_/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="_/css/myOverrides.css">
+    <link rel="icon" type="icon" href="_/img/favicon.ico">
+  </head>
+
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script type="text/javascript" src="_/js/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="_/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="_/js/myScripts.js"></script>
+  </body>
+</html>
